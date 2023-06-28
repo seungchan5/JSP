880400c90cb596ebc743c4afbdbed99a793a7061
seungchan <starosc37@gmail.com>
2023-06-28 14:53:53 +0900

    0628

--- library_0427/src/com/library/service/BookService.java
+++ library_0427/src/com/library/service/BookService.java
@@ -38,15 +38,11 @@ public class BookService {
 	/**
 	 * 도서 정보 입력
 	 */
-	public void insert(String title, String author) {
-		Book book = new Book(title, author);
+	public int insert(Book book) {
+		
 		int res = dao.insert(book);
-		if(res > 0) {
-			System.out.println(res + "건 입력 되었습니다.");
-		} else {
-			System.err.println("입력중 오류가 발생 하였습니다.");
-			System.err.println("관리자에게 문의 해주세요");
-		}
+		return res;
+	
 	}
 
 	public int delete(String noStr) {
@@ -54,24 +50,9 @@ public class BookService {
 		return res;
 	}
 
-	public void rentBook(int bookNo) {
-		// 대여가능한 도서인지 확인
-		String rentYN = dao.getRentYN(bookNo);
-		if("Y".equals(rentYN)) {
-			System.err.println("이미 대여중인 도서 입니다.");
-		} else if ("".equals(rentYN)) {
-			System.out.println("없는 도서 번호 입니다.");
-		}
-		
-		// 대여처리
-		int res = dao.update(bookNo, "Y");
-		
-		if(res>0) {
-			System.out.println(res + "건 대여 되었습니다.");
-		}else {
-			System.out.println("대여중 오류가 발생 하였습니다.");
-			System.out.println("관리자에게 문의 해주세요");
-		}
+	public int rentBook(Book book) {
+		int res = dao.rentBook(book);
+		return res;
 	}
 
 	public void returnBook(int bookNo) {
@@ -93,6 +74,11 @@ public class BookService {
 			System.out.println("관리자에게 문의 해주세요");
 		}
 	}
+	
+	public Book selectOne(String no) {
+		return dao.selectOne(no);
+	}
+
 	
 }
 
